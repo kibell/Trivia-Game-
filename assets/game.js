0ed46db850575eb222b636230dba45db21b12e94
Kitrick Bell <kitrickd.bell@gmail.com>
2019-10-21 19:16:35 -0500

    added pseudocode to the js file to give me an idea of what I need for the game

---
+++ assets/game.js
@@ -0,0 +1,23 @@
+// Declare the variables need for the game
+
+
+//an Array of Questions and answers
+
+
+//Create a timer that will countdown
+
+
+
+// what should  happen when the timer runs out
+
+
+
+//reset
+
+//a function that will run through the questions after each selection
+
+
+// a function that calls the array of questions and adds a point when you get the answer wrong
+
+
+
